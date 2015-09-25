bc33ee118a703f0860c3d9ccd8f8da0fd635ad65
Steve Judd <sejudd1@github.com>
2015-09-25 14:00:37 -0700

    adds shop model index action

---
+++ app/models/Shop.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//set up Shop Schema
+var ShopSchema = new Schema({
+	shopname: { type: String, required: true, index: { unique: true }},
+	street: { type: String, required: true},
+	city: { type: String, required: true },
+	phonenumber: String,
+	hours: String
+
+
+})
+
+
+
+module.exports = mongoose.model('Shop', ShopSchema)
---
+++ server.js
@@ -0,0 +1,50 @@
+var express = require('express'), 
+	app = express(),
+	bodyParser = require('body-parser'),
+	morgan = require('morgan'),
+	mongoose = require( 'mongoose' ),
+	Shop = require( './app/models/Shop.js' );
+	// Deck = require( './app/models/Deck.js' );
+
+	mongoose.connect( 'localhost:27017/decks_local_app' )
+
+//App configuration
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan( 'dev' ))
+
+
+
+
+//Routes
+
+//Create a router for the API
+//set index for api router
+// var apiRouter = express.Router();
+
+// apiRouter.route('/shops')
+// 	.get(function(req, res){
+// 		Shop.find( function(err , shops ){
+// 			if ( err ) {
+// 				res.send( err )
+// 			}
+// 			res.json(users)
+// 		})
+// 	})
+
+//tell app to use API route when we go to localhost:8080/api
+
+
+//Run server
+app.listen(8080)
+console.log('Magic is happening on port 8080')
+
+
+
+
+
+
+
+
+	
