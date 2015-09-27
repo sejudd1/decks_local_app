f0e30dfda519def466ead27e7b2f65dfc37b575c
Steve Judd <sejudd1@github.com>
2015-09-27 12:54:33 -0700

    adds board routes too routes folder

---
+++ app/routes/board.Routes.js
@@ -0,0 +1,32 @@
+var express = require('express')
+	apiRouter = express.Router()
+	boardsController = require('../controllers/boardsController')
+	Board = require('../models/Board.js')
+
+apiRouter.use(function(req, res, next){
+	//Do Authenication Here!
+	console.log('Someone is using our API, we will authenticate them here')
+	//if not logged in res.json({message: "please log in"})
+	//else, next ()
+	next()
+})
+
+//set up index/get for api router
+apiRouter.route('/boards')
+	.get(boardsController.index)
+	//for creating a new board
+	.post(boardsController.create)
+
+apiRouter.route('boards/:boards_id')
+	//this is the show action
+	.get(boardsController.show)
+
+	//this is the update action
+	.put(boardsController.update)
+
+	//this is the desstroy function
+	.delete(usersController.destroy)
+
+module.exports = apiRouter
+
+
