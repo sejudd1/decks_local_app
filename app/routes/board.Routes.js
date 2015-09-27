dda76c86dc95c3fed05aa9c8503f0a2c8a1593ff
Steve Judd <sejudd1@github.com>
2015-09-27 13:23:56 -0700

    fixes spacing for styleguide

--- app/routes/board.Routes.js
+++ app/routes/board.Routes.js
@@ -1,23 +1,23 @@
-var express = require('express')
+var express = require( 'express' )
 	apiRouter = express.Router()
-	boardsController = require('../controllers/boardsController')
-	Board = require('../models/Board.js')
+	boardsController = require( '../controllers/boardsController' )
+	Board = require( '../models/Board.js' )
 
 apiRouter.use(function(req, res, next){
 	//Do Authenication Here!
-	console.log('Someone is using our API, we will authenticate them here')
+	console.log( 'Someone is using our API, we will authenticate them here' ) 
 	//if not logged in res.json({message: "please log in"})
 	//else, next ()
 	next()
 })
 
 //set up index/get for api router
-apiRouter.route('/boards')
+apiRouter.route( '/boards' )
 	.get(boardsController.index)
 	//for creating a new board
 	.post(boardsController.create)
 
-apiRouter.route('boards/:boards_id')
+apiRouter.route( 'boards/:boards_id' )
 	//this is the show action
 	.get(boardsController.show)
 
