f4fb4db824e1b5eca1395c083e7fcb6793d9be8d
Dresosh <contact@dresosh.com>
2015-09-27 21:25:54 -0700

    cleaned up code and added routes

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
-var express = require('express'),
-	app = express(),
-	bodyParser = require('body-parser'),
-	morgan = require('morgan'),
-	mongoose = require( 'mongoose' ),
-	Shop = require( './app/models/Shop.js' ),
-	Board = require( './app/models/Board.js' ),
-	apiRouter = require( './app/routes/board.Routes.js' ),
-	apiRouter2 = require( './app/routes/shop.Routes.js' );
+var express  = require( 'express' ),
+	app 			 = express(),
+	bodyParser = require( 'body-parser' ),
+	morgan 		 = require( 'morgan' ),
+	mongoose 	 = require( 'mongoose' ),
+	Shop 			 = require( './app/models/Shop.js' ),
+	Board 		 = require( './app/models/Board.js' ),
+	apiRouter  = require( './app/routes/board.Routes.js' ),
+ 	apiRouter2 = require( './app/routes/shop.Routes.js' );
 
 
-	mongoose.connect( 'localhost:27017/decks_local_app' )
+mongoose.connect( 'localhost:27017/decks_local_app' );
+
 
 //App configuration
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(morgan( 'dev' ))
+app.use(morgan( 'dev' ));
 
 
 // tell app to use apiRoute when we go to the localhost:8080/api
-app.use('/api', apiRouter)
+app.use( '/api', apiRouter );
+app.use( '/api', apiRouter2 );
 
 //Run server
 app.listen(8080)
-console.log('Magic is happening on port 8080')
+console.log( 'Magic is happening on port 8080' )
